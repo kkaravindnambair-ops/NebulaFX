9196aa6487062b8238a57cc2566f26d7e244925b
kkaravindnambair-ops <kkaravindnambair@gmail.com>
2026-02-11 17:29:55 +0000

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {"message": "Nebula FX Backend Running"}
+    return {"message": "Nebula FX Backend Running 🚀"}
+
+@app.route("/fx-rate")
+def get_fx_rate():
+    # Demo API (we will replace later with real one)
+    url = "https://api.exchangerate.host/latest?base=USD&symbols=INR"
+
+    response = requests.get(url)
+    data = response.json()
+
+    return jsonify(data)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
